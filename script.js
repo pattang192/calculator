1a9caa6484647783df9c65cabce3d10658a04775
Patrila Tangwell <111756874+pattang192@users.noreply.github.com>
2024-12-10 11:26:42 -0400

    Write function that populates display

--- script.js
+++ script.js
@@ -1,30 +1,50 @@
+//Create onclick functions in global scope, then call it in Populate display or relevant function
+
+let num1 = 0;
+let num2 = 0;
+let operator;
+let displayValue;
+let display = document.querySelector(".display");
+const numBtns = document.querySelector(".number-buttons");
+const operatorBtns = document.querySelectorAll(".operator-buttons > div > button");
+
+let number = document.querySelectorAll(".number-buttons > button");
+
+//Create function that populates display
+const populateDisplay = function() {
+    display.textContent += event.target.textContent;
+    return +(display.textContent);
+}
+
+//Create basic calculator functions
 const add = function (x, y) {
     return x + y;
 }
-console.log(add(2, 3));
-
 const subtract = function (x, y) {
     return x - y;
 }
-console.log(subtract(10, 4));
 
 const multiply = function (x, y) {
     return x * y;
 }
-console.log(multiply(5 , 3));
 
 const divide = function (x, y) {
     return x / y;
 }
-console.log(divide(10, 2))
 
-let num1;
-let num2;
-let operator;
-
-const operate = function (x, operator, y) {
-if (operator = '+') add(x, y);
-if (operator = '-') subtract (x, y);
-if (operator = '*') multiply (x, y);
-if (operator = '/') divide (x, y);
+const operate = function (num1, operator, num2) {
+if (operator === '+') add(num1, num2);
+if (operator === '-') subtract (num1, num2);
+if (operator === '*') multiply (num1, num2);
+if (operator === '/') divide (num1, num2);
 }
+operate();
+
+
+
+
+
+
+
+
+
