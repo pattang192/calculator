1dec99fd9906d2856ee5ea2a0326ce2b3a97d4de
Patrila Tangwell <111756874+pattang192@users.noreply.github.com>
2024-12-13 10:07:03 -0400

    Add event listeners to equal and clear buttons

--- script.js
+++ script.js
@@ -2,17 +2,20 @@ let num1 = 0;
 let num2 = 0;
 let operator;
 let displayValue;
-let display = document.querySelector(".display");
-console.log(display)
+const display = document.querySelector(".display");
+let placeholder = document.querySelector(".display").placeholder;
 const numBtns = document.querySelector(".number-buttons");
 const operatorBtns = document.querySelectorAll(".operator-buttons > div > button");
+const equal = document.querySelector(".equal-button");
+const clear = document.querySelector(".clear-button");
+let result = 0;
 
 let number = document.querySelectorAll(".number-buttons > button");
 
 //Create function that populates display
 const populateDisplay = function() {
     display.value += event.target.textContent;
-    return +(display.value);
+    return display.value;
 }
 
 //Add on-click function to number buttons
@@ -21,40 +24,61 @@ for (const num of number) {
 }
 
 //Create basic calculator functions
-
-//Create result variable that updates with the result 
-//of any operation. 
-//Update functions to return result 
 const add = function (x, y) {
-    return x + y;
+    result = x + y;
+    return result;
 }
 const subtract = function (x, y) {
-    return x - y;
+    result = x - y;
+    return result;
 }
 
 const multiply = function (x, y) {
-    return x * y;
+    result = x * y;
+    return result;
 }
 
 const divide = function (x, y) {
-    return x / y;
+    result = x / y;
+    return result;
 }
 
-const operate = function (num1, operator, num2) {
-    operatorBtns.forEach((button) => {
-        button.addEventListener('click', () => {
-            displayValue = display.value;
-            num1 = displayValue;
-            operator = event.target.textContent;
-            alert(num1);
-        })
+//Add on click function to operator buttons
+operatorBtns.forEach((button) => {
+    button.addEventListener('click', () => {
+        displayValue = +display.value;
+        num1 = displayValue;
+        console.log(num1);
+        console.log(typeof num1)
+        operator = event.target.textContent;
+        console.log(operator)
+        display.value = '';
     })
-if (operator === '+') add(num1, num2);
-if (operator === '-') subtract (num1, num2);
-if (operator === '*') multiply (num1, num2);
-if (operator === '/') divide (num1, num2);
+})
+
+equal.addEventListener('click', () => {
+    displayValue = +display.value;
+    num2 = displayValue;
+    console.log(num2);
+    console.log(typeof num2)
+    display.value = operate(num1, operator, num2);
+})
+
+clear.addEventListener('click', () => {
+    display.value = '';
+    num1 = 0;
+    num2 = 0;
+    operator = '';
+})
+
+
+const operate = function (num1, operator, num2) {
+if (operator === '+') return add(num1, num2);
+if (operator === '-') return subtract (num1, num2);
+if (operator === '*') return multiply (num1, num2);
+if (operator === '/') return divide (num1, num2);
 }
-operate();
+
 
 
 
