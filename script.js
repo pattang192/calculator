2a762dc78b357b19f002f670a4e913e0138ec543
Patrila Tangwell <111756874+pattang192@users.noreply.github.com>
2024-12-16 15:29:45 -0400

    Add event listener to decimal and arithmetic sign buttons

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ let num2 = 0;
 let operator;
 let displayValue;
 const display = document.querySelector(".display");
-let placeholder = document.querySelector(".display").placeholder;
-const numBtns = document.querySelector(".number-buttons");
+const number = document.querySelectorAll(".number-panel > .number");
+const decimalPoint = document.querySelector(".number-panel > .decimal")
+const arithmeticSign = document.querySelector(".number-panel > .arithmetic-sign")
 const operatorBtns = document.querySelectorAll(".operator-buttons > div > button");
 const equal = document.querySelector(".equal-button");
 const clear = document.querySelector(".clear-button");
 let result = 0;
 
-let number = document.querySelectorAll(".number-buttons > button");
 
 //Create function that populates display
 const populateDisplay = function() {
@@ -23,11 +23,27 @@ for (const num of number) {
     num.addEventListener('click', populateDisplay);
 }
 
+//Add on-click function to decimal button
+decimalPoint.addEventListener('click', () => {
+    displayValue = display.value;
+    if (!displayValue.includes(".")) {
+        display.value += ".";
+    }
+})
+
+//Add on-click function to arithmetic sign
+arithmeticSign.addEventListener('click', () => {
+    displayValue = +display.value;
+    displayValue = -displayValue;
+    display.value = displayValue;
+})
+
 //Create basic calculator functions
 const add = function (x, y) {
     result = x + y;
     return result;
 }
+
 const subtract = function (x, y) {
     result = x - y;
     return result;
@@ -45,22 +61,20 @@ const divide = function (x, y) {
 
 //Add on click function to operator buttons
 operatorBtns.forEach((button) => {
+//Add lines that call appropriate operator function on-click
+//that will operate on num1 and display.value
     button.addEventListener('click', () => {
         displayValue = +display.value;
         num1 = displayValue;
-        console.log(num1);
-        console.log(typeof num1)
         operator = event.target.textContent;
-        console.log(operator)
         display.value = '';
+        document.querySelector(".display").placeholder = num1;
     })
 })
 
 equal.addEventListener('click', () => {
     displayValue = +display.value;
     num2 = displayValue;
-    console.log(num2);
-    console.log(typeof num2)
     display.value = operate(num1, operator, num2);
 })
 
@@ -69,8 +83,8 @@ clear.addEventListener('click', () => {
     num1 = 0;
     num2 = 0;
     operator = '';
+    document.querySelector(".display").placeholder = 0;
 })
-
 
 const operate = function (num1, operator, num2) {
 if (operator === '+') return add(num1, num2);
@@ -78,13 +92,3 @@ if (operator === '-') return subtract (num1, num2);
 if (operator === '*') return multiply (num1, num2);
 if (operator === '/') return divide (num1, num2);
 }
-
-
-
-
-
-
-
-
-
-
